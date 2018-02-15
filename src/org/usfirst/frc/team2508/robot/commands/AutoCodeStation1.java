386366f3c4fcb20eb9be6f63334bf58b25a063ea
SteveFranklin <steve11sjf@gmail.com>
2018-02-15 16:27:03 -0600

    Added the left and right auto codes
    Still need to test

--- src/org/usfirst/frc/team2508/robot/commands/AutoCodeStation1.java
+++ src/org/usfirst/frc/team2508/robot/commands/AutoCodeStation1.java
@@ -1,24 +1,58 @@
 package org.usfirst.frc.team2508.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.DriverStation;
+import org.usfirst.frc.team2508.robot.Robot;
+import org.usfirst.frc.team2508.robot.RobotMap;
 
-public class AutoCodeStation1 extends Command {
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-	String gameData;
-	
-	public AutoCodeStation1() {
-		gameData = DriverStation.getInstance().getGameSpecificMessage();
+public class AutoCodeStation1 extends CommandGroup {
+	public AutoCodeStation1()
+	{
+		if(Robot.switchPositions.charAt(0) == 'L')
+		{
+			// forward 32
+			addSequential(new DriveMotionMagic(32, 32));
+			// turn 90 CCW
+			addSequential(new DriveMotionMagic(0, 16.3));
+			// forward 15
+			addSequential(new DriveMotionMagic(15, 15));
+			// turn 90 CW
+			addSequential(new DriveMotionMagic(16.3, 0));
+			// forward 100
+			addSequential(new DriveMotionMagic(100, 100));
+			// turn 90 CW
+			addSequential(new DriveMotionMagic(16.3, 0));
+			// forward 50.69
+			addSequential(new DriveMotionMagic(50.7, 50.7));
+			// lift cube
+			//addSequential(new MoveCube(RobotMap.SwitchPlacePosition));
+			// release cube
+			addSequential(new ReleaseCube());
+		}
+		if(Robot.switchPositions.charAt(0) == 'R')
+		{
+			// forward 32
+			addSequential(new DriveMotionMagic(32, 32));
+			// turn 90 CCW
+			addSequential(new DriveMotionMagic(0, 16.3));
+			// forward 15
+			addSequential(new DriveMotionMagic(15, 15));
+			// turn 90 CW
+			addSequential(new DriveMotionMagic(16.3, 0));
+			// forward 140
+			addSequential(new DriveMotionMagic(140, 140));
+			// turn 90 CW
+			addSequential(new DriveMotionMagic(16.3, 0));
+			// forward 181.1
+			addSequential(new DriveMotionMagic(181.1, 181.1));
+			// turn 90 CW
+			addSequential(new DriveMotionMagic(16.3, 0));
+			// forward 12
+			addSequential(new DriveMotionMagic(12, 12));
+			// lift cube
+			//addSequential(new MoveCube(RobotMap.SwitchPlacePosition));
+			// release cube
+			addSequential(new ReleaseCube());
+		}
 	}
-	
-	protected void initialize() {
-		
-	}
-	
-	@Override
-	protected boolean isFinished() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
--- src/org/usfirst/frc/team2508/robot/commands/AutoCodeStation3.java
+++ src/org/usfirst/frc/team2508/robot/commands/AutoCodeStation3.java
@@ -1,13 +1,58 @@
 package org.usfirst.frc.team2508.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team2508.robot.Robot;
+import org.usfirst.frc.team2508.robot.RobotMap;
 
-public class AutoCodeStation3 extends Command {
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-	@Override
-	protected boolean isFinished() {
-		// TODO Auto-generated method stub
-		return false;
+public class AutoCodeStation3 extends CommandGroup {
+	public AutoCodeStation3()
+	{
+		if(Robot.switchPositions.charAt(0) == 'L')
+		{
+			// forward 32
+			addSequential(new DriveMotionMagic(32, 32));
+			// turn 90 CW
+			addSequential(new DriveMotionMagic(16.3, 0));
+			// forward 15
+			addSequential(new DriveMotionMagic(15, 15));
+			// turn 90 CCW
+			addSequential(new DriveMotionMagic(0, 16.3));
+			// forward 140
+			addSequential(new DriveMotionMagic(140, 140));
+			// turn 90 CCW
+			addSequential(new DriveMotionMagic(0, 16.3));
+			// forward 181.1
+			addSequential(new DriveMotionMagic(181.1, 181.1));
+			// turn 90 CCW
+			addSequential(new DriveMotionMagic(0, 16.3));
+			// forward 12
+			addSequential(new DriveMotionMagic(12, 12));
+			// lift cube
+			//addSequential(new MoveCube(RobotMap.SwitchPlacePosition));
+			// release cube
+			addSequential(new ReleaseCube());
+		}
+		if(Robot.switchPositions.charAt(0) == 'R')
+		{
+			// forward 32
+			addSequential(new DriveMotionMagic(32, 32));
+			// turn 90 CW
+			addSequential(new DriveMotionMagic(16.3, 0));
+			// forward 15
+			addSequential(new DriveMotionMagic(15, 15));
+			// turn 90 CCW
+			addSequential(new DriveMotionMagic(0, 16.3));
+			// forward 100
+			addSequential(new DriveMotionMagic(100, 100));
+			// turn 90 CCW
+			addSequential(new DriveMotionMagic(0, 16.3));
+			// forward 50.69
+			addSequential(new DriveMotionMagic(50.7, 50.7));
+			// lift cube
+			//addSequential(new MoveCube(RobotMap.SwitchPlacePosition));
+			// release cube
+			addSequential(new ReleaseCube());
+		}
 	}
-
 }
