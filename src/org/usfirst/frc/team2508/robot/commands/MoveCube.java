7c077b04a3d5529549dd235ae40ecacbed584a04
SteveFranklin <steve11sjf@gmail.com>
2018-02-16 18:29:26 -0600

    Now gets the switch side from driver station

--- src/org/usfirst/frc/team2508/robot/commands/MoveCube.java
+++ src/org/usfirst/frc/team2508/robot/commands/MoveCube.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team2508.robot.commands;
 
 import org.usfirst.frc.team2508.robot.Robot;
-import org.usfirst.frc.team2508.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
